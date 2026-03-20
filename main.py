ac7a5ac2f1bd3209c3261d2f09c8e7ad028b39b7
Rodrigo <domingos.drigo@gmail.com>
2026-03-20 15:33:27 -0400

    tavily

--- main.py
+++ main.py
@@ -2,37 +2,39 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain.agents import create_agent
-from langchain_openai import ChatOpenAI
-from langchain_tavily import TavilySearch
 
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
+from langchain.agents import create_agent
+from langchain.tools import tool
+#from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+from langchain_core.messages import HumanMessage
 from schemas import AgentResponse
 
-tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4o")
+# tavilty = TavilyClient()
 
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tool that searchs over internet
+#     Args:query (str): The query to search for
+#     Returns:
+#     The serch result
+#     """
+#     print(f"Searching for {query}")   
+#     return tavilty.search(query=query)
 
-agent = create_agent(
-    model=llm,
-    tools=tools,
-    response_format=AgentResponse,
-)
+llm= ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7)
 
+tools = [TavilySearch]
+agent = create_agent(llm, tools)
 
 def main():
-    result = agent.invoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
-                }
-            ]
-        }
-    )
-    # Access structured response from the agent
-    structured = result.get("structured_response", None)
-    print(structured if structured is not None else result)
+    print("Hello form langchain-course!")
+    result = agent.invoke({"messages":HumanMessage(content="Search for top 3 skills for AI engineer in 2026 to get a job on linkedin open jobs")})
+    print(result['messages'][-1].content)
 
 
 if __name__ == "__main__":
